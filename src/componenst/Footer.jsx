b3566ecf39acafb35d0c80a23d80d488c0fd1d33
Studente <andreeamusteata68@gmail.com>
2023-02-23 12:51:09 +0100

    seconda lezione

--- src/componenst/Footer.jsx
+++ src/componenst/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
 return(
     <footer>
         <div className='footer-conteiner'>
-            <div>selction1 </div>
-            <div>selction2 </div>
-            <div >selction3</div>
-            <div >selction4</div>
+            <div>selction1</div>
+            <div>selction2</div>
+            <div>selction3</div>
+            <div>selction4</div>
         </div>
     </footer>
 )
